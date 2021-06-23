465f1f8489fd5242a9f005c2ebe14d0e949d5d08
Pranjal <pranjal.chanda@tataelxsi.co.in>
2021-06-23 16:50:47 +0530

    + Main modification + Screenshot added

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import cv2
 import HandTracking as ht
 
-# Capture the video stream Webcam
-cap = cv2.VideoCapture(0)
+def main():    
+    # Capture the video stream Webcam
+    cap = cv2.VideoCapture(0)
 
-track = ht.HandTracking()
+    track = ht.HandTracking()
+    # Infinite loop waiting for key 'q' to terminate
+    while cv2.waitKey(1) != (ord('q') or ord('Q')):
+        # Read the frame
+        success, img = cap.read()
+        # Flip input image horizontally
+        flipImage = cv2.flip(img, 1)
+        # Track and revert the image
+        track.findHands(flipImage, showFps = True)
+        # Show the resultant image
+        cv2.imshow("Output", flipImage)
 
-# Infinite loop waiting for key 'q' to terminate
-while cv2.waitKey(1) != (ord('q') or ord('Q')):
-
-    # Read the frame
-    success, img = cap.read()
-    # Flip input image horizontally
-    flipImage = cv2.flip(img, 1)
-
-    track.findHands(flipImage)
-
-    cv2.imshow("Output", flipImage)
+if __name__ == '__main__':
+    main()
