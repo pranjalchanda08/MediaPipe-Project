85895260924c3e525c21770f5c05ba26f401a5a7
Pranjal <pranjal.chanda@tataelxsi.co.in>
2021-06-23 13:16:07 +0530

    + FPS

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ cap = cv2.VideoCapture(0)
 
 # Load the hand model
 mpHand = mp.solutions.hands
-
 # Load mediapipe drawing utility
 mpDraw = mp.solutions.drawing_utils
-
+# Create hand object
 hands = mpHand.Hands(
     static_image_mode        = False,     # If True the whole time it will perform detection
-    max_num_hands            = 2,
-    min_detection_confidence = 0.5,
+    max_num_hands            = 4,
+    min_detection_confidence = 0.7,
     min_tracking_confidence  = 0.7
     )
+
+fps   = 0
+ctime = 0
+ptime = 0
 
 # Infinite loop waiting for key 'q' to terminate
 while cv2.waitKey(1) != (ord('q') or ord('Q')):
@@ -29,13 +32,30 @@ while cv2.waitKey(1) != (ord('q') or ord('Q')):
     imgRGB = cv2.cvtColor(flipImage, cv2.COLOR_BGR2RGB)
     # Process the RGB image using the model
     results = hands.process(imgRGB)
-    # Print landmarks
-    # print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
     # Itterate over all landmarks
         for landmarks in results.multi_hand_landmarks:
+            # print(results.multi_hand_landmarks)
             # Draw the landmark points on image
-            mpDraw.draw_landmarks(flipImage, landmarks, mpHand.HAND_CONNECTIONS)
+            mpDraw.draw_landmarks(
+                flipImage, 
+                landmarks, 
+                mpHand.HAND_CONNECTIONS
+                )
+    # Calculate FPS
+    ctime = time.time()
+    fps   = 1/(ctime - ptime)
+    ptime = ctime 
 
+    # Include FPS text in image
+    cv2.putText(
+        flipImage, 
+        "FPS: {}".format(int(fps)), 
+        (10,70),                        # Position
+        cv2.FONT_HERSHEY_PLAIN,
+        1,                              # Font size
+        (0,255,0),
+        1                               # Thickness
+        )
     cv2.imshow("Output", flipImage)
