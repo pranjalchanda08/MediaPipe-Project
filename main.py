6ed62216a5d2331cd6d521bb09abacdd3884b16d
Pranjal <pranjal.chanda@tataelxsi.co.in>
2021-06-23 10:48:19 +0530

    + Media pipe process.

--- main.py
+++ main.py
@@ -2,8 +2,27 @@ import cv2
 import mediapipe as mp
 import time
 
+# Capture the video stream Webcam
 cap = cv2.VideoCapture(0)
 
+# Load the model
+mpHand = mp.solutions.hands
+
+hands = mpHand.Hands(
+	static_image_mode        = False,     # If True the whole time it will perform detection
+	max_num_hands            = 2,
+	min_detection_confidence = 0.5,
+	min_tracking_confidence  = 0.7
+	)
 while cv2.waitKey(1) != ord('q'):
+	
+	# Read the frame
 	success, img = cap.read()
+	# Convert the image to RGB
+	imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	# Process the RGB image using the model
+	results = hands.process(imgRGB)
+	# Print landmarks
+	print(results.multi_hand_landmarks)
+	
 	cv2.imshow("Capture", img)
