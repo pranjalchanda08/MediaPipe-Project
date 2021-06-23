27b9b9a2062ba4ecad657b1fcdca6a55552d9426
Pranjal <pranjal.chanda@tataelxsi.co.in>
2021-06-23 11:23:50 +0530

    + Flip input image + Drawing utils

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import time
 # Capture the video stream Webcam
 cap = cv2.VideoCapture(0)
 
-# Load the model
+# Load the hand model
 mpHand = mp.solutions.hands
+
+# Load mediapipe drawing utility
+mpDraw = mp.solutions.drawing_utils
 
 hands = mpHand.Hands(
 	static_image_mode        = False,     # If True the whole time it will perform detection
@@ -14,15 +17,25 @@ hands = mpHand.Hands(
 	min_detection_confidence = 0.5,
 	min_tracking_confidence  = 0.7
 	)
-while cv2.waitKey(1) != ord('q'):
+
+# Infinite loop waiting for key 'q' to terminate
+while cv2.waitKey(1) != (ord('q') or ord('Q')):
 	
 	# Read the frame
 	success, img = cap.read()
+	# Flip input image horizontally
+	flipImage = cv2.flip(img, 1)
 	# Convert the image to RGB
-	imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	imgRGB = cv2.cvtColor(flipImage, cv2.COLOR_BGR2RGB)
 	# Process the RGB image using the model
 	results = hands.process(imgRGB)
 	# Print landmarks
-	print(results.multi_hand_landmarks)
+	# print(results.multi_hand_landmarks)
 	
-	cv2.imshow("Capture", img)
+	if results.multi_hand_landmarks:
+	# Itterate over all landmarks
+		for landmarks in results.multi_hand_landmarks:
+			# Draw the landmark points on image
+			mpDraw.draw_landmarks(flipImage, landmarks)
+
+	cv2.imshow("Output", flipImage)
