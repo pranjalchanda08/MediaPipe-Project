e76f62b42c600e2760191704e12f8de9245874e0
Pranjal <pranjal.chanda@tataelxsi.co.in>
2021-06-23 14:16:00 +0530

    + Identify Finger tip landmarks.

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import cv2
 import mediapipe as mp
 import time
 
+# List of available landmarks
+LANDMARK = [
+    'WRIST',        
+    'THUMB_CMC',    'THUMP_MCP',    'THUMB_IP',     'THUMB_TIP',
+    'INDEX_MCP',    'INDEX_PIP',    'INDEX_DIP',    'INDEX_TIP',
+    'MIDDLE_MCP',   'MIDDLE_PIP',   'MIDDLE_DIP',   'MIDDLE_TIP',
+    'RING_MCP',     'RING_PIP',     'RING_DIP',     'RING_TIP',
+    'PINKY_MCP',    'PINKY_PIP',    'PINKY_DIP',    'PINKY_TIP'
+]
+
+# Make a dictionary of landmarks with its index
+LM_DICT = {lm:LANDMARK.index(lm) for lm in LANDMARK}
+
 # Capture the video stream Webcam
 cap = cv2.VideoCapture(0)
 
@@ -36,7 +49,23 @@ while cv2.waitKey(1) != (ord('q') or ord('Q')):
     if results.multi_hand_landmarks:
     # Itterate over all landmarks
         for landmarks in results.multi_hand_landmarks:
-            # print(results.multi_hand_landmarks)
+            for index_, lm in enumerate(landmarks.landmark):
+                # Get the height and width of image
+                h, w, c = flipImage.shape
+                # Calculate the position of the landmark in the picture
+                cx , cy = int(w * lm.x), int(h * lm.y)
+                # Check if the landmark is present in the index list
+                if index_ in [
+                    LM_DICT['WRIST'],
+                    LM_DICT['THUMB_TIP'], 
+                    LM_DICT['INDEX_TIP'], 
+                    LM_DICT['MIDDLE_TIP'], 
+                    LM_DICT['RING_TIP'], 
+                    LM_DICT['PINKY_TIP']
+                ]:
+                    # Draw circles around the detections
+                    cv2.circle(flipImage, (cx,cy), 15, (255,0,255), cv2.FILLED)
+
             # Draw the landmark points on image
             mpDraw.draw_landmarks(
                 flipImage, 
@@ -49,8 +78,7 @@ while cv2.waitKey(1) != (ord('q') or ord('Q')):
     ptime = ctime 
 
     # Include FPS text in image
-    cv2.putText(
-        flipImage, 
+    cv2.putText(flipImage, 
         "FPS: {}".format(int(fps)), 
         (10,70),                        # Position
         cv2.FONT_HERSHEY_PLAIN,
