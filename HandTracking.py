2a7c3093e541496dec7545ee526134873cb6397a
Pranjal <pranjalchanda08@gmail.com>
2021-06-24 14:49:43 +0530

    + Pycharm setup

--- HandTracking.py
+++ HandTracking.py
@@ -2,64 +2,64 @@ import cv2
 import mediapipe as mp
 import time
 
-class HandTracking():
+
+class HandTracking:
     LANDMARK = [
         'WRIST',
-        'THUMB_CMC',    'THUMP_MCP',    'THUMB_IP',     'THUMB_TIP',
-        'INDEX_MCP',    'INDEX_PIP',    'INDEX_DIP',    'INDEX_TIP',
-        'MIDDLE_MCP',   'MIDDLE_PIP',   'MIDDLE_DIP',   'MIDDLE_TIP',
-        'RING_MCP',     'RING_PIP',     'RING_DIP',     'RING_TIP',
-        'PINKY_MCP',    'PINKY_PIP',    'PINKY_DIP',    'PINKY_TIP'
+        'THUMB_CMC', 'THUMP_MCP', 'THUMB_IP', 'THUMB_TIP',
+        'INDEX_MCP', 'INDEX_PIP', 'INDEX_DIP', 'INDEX_TIP',
+        'MIDDLE_MCP', 'MIDDLE_PIP', 'MIDDLE_DIP', 'MIDDLE_TIP',
+        'RING_MCP', 'RING_PIP', 'RING_DIP', 'RING_TIP',
+        'PINKY_MCP', 'PINKY_PIP', 'PINKY_DIP', 'PINKY_TIP'
     ]
 
     def __init__(
-                self,
-                static_image_mode        = False,     # If True the whole time it will perform detection
-                max_num_hands            = 4,
-                min_detection_confidence = 0.7,
-                min_tracking_confidence  = 0.7
-                ):
-        '''
+            self,
+            static_image_mode=False,  # If True the whole time it will perform detection
+            max_num_hands=4,
+            min_detection_confidence=0.7,
+            min_tracking_confidence=0.7
+    ):
+        """
             Initializes the HandTracking module
-        '''
+        """
         self.results = None
         # Make a dictionary of landmarks with its index
-        self.LM_DICT = {lm:HandTracking.LANDMARK.index(lm) for lm in HandTracking.LANDMARK}
+        self.LM_DICT = {lm: HandTracking.LANDMARK.index(lm) for lm in HandTracking.LANDMARK}
         # Load the hand_landmarks model
         self.mpHand = mp.solutions.hands
         # Load mediapipe drawing utility
         self.mpDraw = mp.solutions.drawing_utils
         # Create hand_landmarks object
-        self.hands  = self.mpHand.Hands(
-            static_image_mode        ,
-            max_num_hands            ,
-            min_detection_confidence ,
+        self.hands = self.mpHand.Hands(
+            static_image_mode,
+            max_num_hands,
+            min_detection_confidence,
             min_tracking_confidence
-            )
+        )
 
-    def findHand( self,
-                  imgBGR,
-                ):
+    def find_hand(self,
+                  imgbgr,
+                  ):
         # Convert the image to RGB
-        imgRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
+        imgRGB = cv2.cvtColor(imgbgr, cv2.COLOR_BGR2RGB)
         # Process the RGB image using the model
         self.results = self.hands.process(imgRGB)
 
-
-    def findFingerTips( self,
-                        imgBGR,
-                        color           = (255,255,255),
-                        show_connected  = False,
-                        show_landmarks  = False,
-                        draw_tips       = True,
-                        finger_list     = None,
-                        hand_id_list    = 0
-                       ):
-        '''
+    def find_finger_tips(self,
+                         imgbgr,
+                         color=(255, 255, 255),
+                         show_connected=False,
+                         show_landmarks=False,
+                         draw_tips=True,
+                         finger_list=None,
+                         hand_id_list=0
+                         ):
+        """
             Finds finger tips location on cv2.BGR image
-        '''
+        """
         try:
-            Finger_points = \
+            finger_points = \
                 [
                     self.LM_DICT['THUMB_TIP'],
                     self.LM_DICT['INDEX_TIP'],
@@ -67,65 +67,65 @@ class HandTracking():
                     self.LM_DICT['RING_TIP'],
                     self.LM_DICT['PINKY_TIP']
                 ]
-            cx , cy = None, None
-            return_dict = {str(hand_id) : [] for hand_id in hand_id_list}
+            cx, cy = None, None
+            return_dict = {str(hand_id): [] for hand_id in hand_id_list}
             if finger_list is not None:
-                Finger_points = [self.LM_DICT[(finger.upper()+'_TIP')] for finger in finger_list]
+                finger_points = [self.LM_DICT[(finger.upper() + '_TIP')] for finger in finger_list]
 
             if self.results.multi_hand_landmarks:
                 # Itterate over all hands
                 for hand_id in hand_id_list:
                     return_dict[str(hand_id)] = []
-                    hand_landmarks = self.results.multi_hand_landmarks [ hand_id ]
+                    hand_landmarks = self.results.multi_hand_landmarks[hand_id]
                     for index_, lm in enumerate(hand_landmarks.landmark):
                         # Get the height and width of image
-                        h, w, c = imgBGR.shape
+                        h, w, c = imgbgr.shape
                         # Calculate the position of the landmark in the picture
-                        cx , cy = int(w * lm.x), int(h * lm.y)
+                        cx, cy = int(w * lm.x), int(h * lm.y)
                         # Check if the landmark is present in the index list
-                        if index_ in Finger_points:
+                        if index_ in finger_points:
                             # Append the x,y coordinates  of the landmark
-                            return_dict[str(hand_id)].append((cx, cy)) 
+                            return_dict[str(hand_id)].append((cx, cy))
                             # Draw circles around the detections
-                            cv2.circle(imgBGR, (cx,cy), 15, color, cv2.FILLED) if draw_tips else None
+                            cv2.circle(imgbgr, (cx, cy), 15, color, cv2.FILLED) if draw_tips else None
                     # Draw the landmark points on image
                     if show_landmarks:
                         self.mpDraw.draw_landmarks(
-                            imgBGR,
+                            imgbgr,
                             hand_landmarks,
                             self.mpHand.HAND_CONNECTIONS if show_connected else None
-                            )
-        except :
+                        )
+        except:
             pass
         finally:
             return return_dict
 
-    def findAllLandmarks(
-                        self,
-                        imgBGR,
-                        hand_id_list    = 0,
-                        finger_list     = None
-                    ):
+    def find_all_landmarks(
+            self,
+            imgbgr,
+            hand_id_list=0,
+            finger_list=None
+    ):
         try:
-            cx , cy = None, None
-            return_dict = {str(hand_id) : [] for hand_id in hand_id_list}
+            cx, cy = None, None
+            return_dict = {str(hand_id): [] for hand_id in hand_id_list}
             if finger_list is not None:
-                Finger_points = [self.LM_DICT[(finger.upper()+'_TIP')] for finger in finger_list]
+                finger_points = [self.LM_DICT[(finger.upper() + '_TIP')] for finger in finger_list]
 
             if self.results.multi_hand_landmarks:
-                # Itterate over all hands
+                # Iterate over all hands
                 for hand_id in hand_id_list:
                     return_dict[str(hand_id)] = []
-                    hand_landmarks = self.results.multi_hand_landmarks [ hand_id ]
+                    hand_landmarks = self.results.multi_hand_landmarks[hand_id]
                     for index_, lm in enumerate(hand_landmarks.landmark):
                         # Get the height and width of image
-                        h, w, c = imgBGR.shape
+                        h, w, c = imgbgr.shape
                         # Calculate the position of the landmark in the picture
-                        cx , cy = int(w * lm.x), int(h * lm.y)
+                        cx, cy = int(w * lm.x), int(h * lm.y)
                         # Check if the landmark is present in the index list
                         # Append the x,y coordinates  of the landmark
-                        return_dict[str(hand_id)].append((cx, cy)) 
-        except :
+                        return_dict[str(hand_id)].append((cx, cy))
+        except Exception as e:
             pass
         finally:
             return return_dict
--- main.py
+++ main.py
@@ -2,52 +2,54 @@ import cv2
 import time
 import HandTracking as ht
 
-def main( showFps = False , video_src = 0):
+
+def main(show_fps=False, video_src=0):
     # Capture the video stream Webcam
-    cap   = cv2.VideoCapture(video_src)
-    ptime = 0
-    ctime = 0
-    fps   = 0
+    cap = cv2.VideoCapture(video_src)
+    previous_time = 0
+    current_time = 0
+    fps = 0
     track = ht.HandTracking()
     # Infinite loop waiting for key 'q' to terminate
     while cv2.waitKey(1) != (ord('q') or ord('Q')):
         # Read the frame
         success, img = cap.read()
         # Flip input image horizontally
-        flipImage = cv2.flip(img, 1)
+        flipimage = cv2.flip(img, 1)
 
         # Track and revert the image
-        track.findHand(flipImage)
-        pos_list_dict = track.findFingerTips(
-                                flipImage,
-                                finger_list     = None,    # Add Finger string list else None
-                                show_connected  = True,
-                                show_landmarks  = True,
-                                draw_tips       = True,
-                                hand_id_list    = [0, 1]
-                            )
-        pos_list_dict = track.findAllLandmarks(
-                                        flipImage,
-                                        hand_id_list = [0, 1],
-                                        finger_list = None
-                                    )
+        track.find_hand(flipimage)
+        pos_list_dict = track.find_finger_tips(
+            flipimage,
+            finger_list=None,  # Add Finger string list else None
+            show_connected=True,
+            show_landmarks=True,
+            draw_tips=True,
+            hand_id_list=[0, 1]
+        )
+        pos_list_dict = track.find_all_landmarks(
+            flipimage,
+            hand_id_list=[0, 1],
+            finger_list=None
+        )
         print(pos_list_dict)
         # Calculate FPS
-        if showFps:
-            ctime = time.time()
-            fps   = 1/(ctime - ptime)
-            ptime = ctime
+        if show_fps:
+            current_time = time.time()
+            fps = 1 / (current_time - previous_time)
+            previous_time = current_time
             # Include FPS text in image
-            cv2.putText(flipImage,
-                "FPS: {}".format(int(fps)),
-                (10,70),                        # Position
-                cv2.FONT_HERSHEY_PLAIN,
-                1,                              # Font size
-                (0,0,255),
-                2                               # Thickness
-                )
+            cv2.putText(flipimage,
+                        "FPS: {}".format(int(fps)),
+                        (10, 70),  # Position
+                        cv2.FONT_HERSHEY_PLAIN,
+                        1,  # Font size
+                        (0, 0, 255),
+                        2  # Thickness
+                        )
         # Show the resultant image
-        cv2.imshow("Output", flipImage)
+        cv2.imshow("Output", flipimage)
+
 
 if __name__ == '__main__':
-    main(showFps = True)
+    main(show_fps=True)
