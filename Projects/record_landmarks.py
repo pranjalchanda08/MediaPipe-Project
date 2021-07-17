059a77403fd73659761ed7fd342f68af577ec59d
Pranjal <pranjalchanda08@gmail.com>
2021-07-17 22:40:37 +0530

    + csv support

--- Projects/record_landmarks.py
+++ Projects/record_landmarks.py
@@ -12,10 +12,11 @@ finally:
 
 
 def pre_process(landmarks):
-    flatten = []
-    for lms in landmarks:
-        for point in lms:
-            flatten.append(point)
+    flatten = {}
+    for index, lms in enumerate(landmarks):
+        flatten[(str(index) + '_x')] = lms[0]
+        flatten[(str(index) + '_y')] = lms[1]
+        flatten[(str(index) + '_z')] = lms[2]
     return flatten
 
 
@@ -28,6 +29,12 @@ def main(show_fps=False, video_src=0, flip: bool = True, csv_src: str = 'record.
     track = ht.HandTracking(min_detection_confidence=0.8,
                             min_tracking_confidence=0.8)
     is_record = True
+    labels = []
+    csv_dict = {'labels': labels}
+    for index in range(0, 21):
+        csv_dict[(str(index) + '_x')] = []
+        csv_dict[(str(index) + '_y')] = []
+        csv_dict[(str(index) + '_z')] = []
     while is_record:
         temp = sample
         label = input("Enter Label: ")
@@ -44,8 +51,11 @@ def main(show_fps=False, video_src=0, flip: bool = True, csv_src: str = 'record.
             finger_up_dict = track.find_raw_landmarks(hand_id_list=[0])
             if len(finger_up_dict['0']):
                 temp -= 1
-                flatten = pre_process(finger_up_dict['0'])
-                print(flatten)
+                labels.append(label)
+                for index, lms in enumerate(finger_up_dict['0']):
+                    csv_dict[(str(index) + '_x')].append(lms[0])
+                    csv_dict[(str(index) + '_y')].append(lms[1])
+                    csv_dict[(str(index) + '_z')].append(lms[2])
             # Calculate FPS
             if show_fps:
                 current_time = time.time()
@@ -66,6 +76,8 @@ def main(show_fps=False, video_src=0, flip: bool = True, csv_src: str = 'record.
         cv2.destroyAllWindows()
         is_record = input("Do you want to record? (y/n)")
         is_record = is_record.upper() == 'Y'
+    csv_opt = pd.DataFrame(csv_dict)
+    csv_opt.to_csv(csv_src)
 
 
 if __name__ == "__main__":
